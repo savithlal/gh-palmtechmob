74c369645f0659a41bf3aef1b5edbc6eef2233d0
savithlal <100188151+savithlal@users.noreply.github.com>
2022-06-28 17:48:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
          <br/>       
          Welcome to Palmtechmobile
+         <br/> 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
