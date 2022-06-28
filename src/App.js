e4325042fbaefe0c5cd79966ee285b27b9271952
savithlal <100188151+savithlal@users.noreply.github.com>
2022-06-28 17:40:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Palmtechmobile
+         <br>       
+         Welcome to Palmtechmobile
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
